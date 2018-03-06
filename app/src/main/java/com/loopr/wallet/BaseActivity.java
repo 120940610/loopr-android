b3bb393e3800d0cc92c1cafbc315d6f7b603f8d5
snow <120940610@qq.com>
2018-03-06 17:24:09 +0800

    create android project for loopring wallet

---
+++ app/src/main/java/com/loopr/wallet/BaseActivity.java
@@ -0,0 +1,16 @@
+package com.loopr.wallet;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by snow on 2018/3/6.
+ */
+
+public class BaseActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/loopr/wallet/MainActivity.java
@@ -0,0 +1,15 @@
+package com.loopr.wallet;
+
+import android.os.Bundle;
+
+/**
+ * Created by snow on 2018/3/6.
+ */
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
